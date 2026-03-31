4a31fa1a28e4ac58583c9617d4fca65e3b443aca
Bari <barahonajose279@gmail.com>
2026-03-31 09:34:39 -0500

    practicas del 26mar

---
+++ practias/practica2.php
@@ -0,0 +1,38 @@
+<html>
+<head>
+    <title>Practica 2</title>
+</head>
+<body>
+    <h1>Practica 2</h1>
+    <?php
+$numero1 = 5;
+$numero2 = 3;
+$resultado = $numero1 + $numero2;
+print ("La suma de $numero1 y $numero2 es: $resultado<br>");
+
+$numero1 = 10;
+$numero2 = 4;
+$resultado = $numero1 - $numero2;
+print ("<br>La resta de $numero1 y $numero2 es: $resultado <br>");
+
+$numero1 = 7;
+$numero2 = 3;
+$resultado = $numero1 * $numero2;
+print ("<br>La multiplicación de $numero1 y $numero2 es: $resultado<br>");
+
+$numero1 = 15;
+$numero2 = 5;
+$resultado = $numero1 / $numero2;
+print ("<br>La división de $numero1 y $numero2 es: $resultado<br>");
+
+$numero1 = 10;
+$numero2 = 3;
+$resultado = $numero1 % $numero2;
+print ("<br>El módulo de $numero1 y $numero2 es: $resultado<br>");
+
+$base = 2;
+$exponente = 3;
+$resultado = $base ** $exponente;
+print ("<br>$base elevado a la potencia de $exponente es: $resultado<br>");
+    ?>
+</body></html>
---
+++ practias/practica3.php
@@ -0,0 +1,17 @@
+<html>
+<head>
+    <title>Practica 3</title>
+</head>
+<body>
+    <h1>Ejemplo de Orden de precedencia</h1>
+    <?php
+    $resultado = 2+3*4;
+    echo "El resultado de 2+3*4 es: $resultado";
+    ?>
+
+    <h1>Ejemplo de Orden de precedencia con parentesis</h1>
+    <?php
+    $resultado = (2+3)*4;
+    echo "El resultado de 2+3*4 es: $resultado";
+    ?>
+</body></html>
